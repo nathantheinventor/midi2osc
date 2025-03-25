8841d2a18e62e8303da0de70434cc3e3e06360e1
Nathan Collins <nathantheinventor@gmail.com>
2025-03-25 19:31:33 -0400

    feat: Set up structure of CLI

---
+++ src/config.py
@@ -0,0 +1,8 @@
+import click
+
+
+@click.command()
+def config():
+    """Command to set up the config file"""
+    click.echo("config")
+    ...
---
+++ src/run.py
@@ -0,0 +1,8 @@
+import click
+
+
+@click.command()
+def run():
+    """Command to run the process"""
+    click.echo("run")
+    ...
